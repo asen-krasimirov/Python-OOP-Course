cc323920df45f2a9d68e982e0982d913effe66c1
asen-krasimirov <68907559+asen-krasimirov@users.noreply.github.com>
2020-11-13 00:10:36 +0200

    5. Inheritance

---
+++ 5. Inheritance/5.2 Exercises/Problem 3- Players and Monsters/project/__init__.py
@@ -0,0 +1,11 @@
+from project.hero import Hero
+from project.elf import Elf
+from project.muse_elf import MuseElf
+from project.wizard import Wizard
+from project.dark_wizard import DarkWizard
+from project.soul_master import SoulMaster
+from project.knight import Knight
+from project.dark_knight import DarkKnight
+from project.blade_knight import BladeKnight
+
+__all__ = ['Hero', 'Elf', 'MuseElf', 'Wizard', 'DarkWizard', 'SoulMaster', 'Knight', 'DarkKnight', 'BladeKnight']
